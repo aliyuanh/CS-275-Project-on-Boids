7e7e201c1176e43874455c2c6d00e7fe3e69df62
amandaliyuan <aliyuanh@gmail.com>
2020-06-09 18:42:53 -0700

    perching added, obstacle semi implemented
    perching is randomized, weights of velocity slightly modified. ParseCV might create rays from the boid to the target (not sure yet). Food is now eaten with foodScript added to the cake. Initial scene modified with assets to be a park with a random cake.

--- Boids/Assets/Scripts/Boid.cs
+++ Boids/Assets/Scripts/Boid.cs
@@ -1,25 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+//using System.Numerics;
+//using System.Numerics;
+//using System.Numerics;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Boid : MonoBehaviour
 {
     public Vector3 velocity;
-
+    //TODO: add tireness + landing boolean trigger :D (20s + math.random*30?)
     private float cohesionRadius = 15;
     private float separationDistance = 6;
     private Collider[] boids;
     private Vector3 cohesion;
     private Vector3 separation;
-    private int separationCount;
     private Vector3 alignment;
-    private Vector3 toFood;
+    public Vector3 toFood;
+    public Vector3 avoidObstacle;
+    public Vector3 toLand;
+    public bool perching;
+    public bool landing;
+    private float timeSincePerch;
     private float maxSpeed = 15;
     private float maxDistance = 45;
+    private float groundHeight = -24f;
+    public float timeUntilTired = 0;
+    List<(string name, Vector3 ray)> objectsInScene;
     public Vector3 origin;
     private void Start()
     {
+        timeSincePerch = 10f;
+        landing = false;
+        perching = false;
+        timeUntilTired = UnityEngine.Random.Range(15f, 75f);
+        objectsInScene = new List<(string name, Vector3 ray)>();
         InvokeRepeating("CalculateVelocity", .01f, .1f);
         Debug.Log(origin);
     }
@@ -93,14 +109,16 @@ public class Boid : MonoBehaviour
                 float angle = Vector3.Angle(myVelocity, targetDir);
                 angle = System.Math.Abs(angle);
                 Debug.Log(angle);
-                if (angle < 60)
-                {
-                    Debug.Log("found food in FOV!");
                     Vector3 toFood = thing.transform.position - transform.position;
                     if (toFood.magnitude < foodVec.magnitude)
                     {
                         foodVec = toFood;
                     }
+                
+                if((transform.position - thing.transform.position).magnitude < 4f)
+                {
+                    FoodScript food = thing.GetComponent<FoodScript>();
+                    food.getPecked();
                 }
             }
         }
@@ -110,13 +128,32 @@ public class Boid : MonoBehaviour
         }
         return foodVec;
     }
+
+    void ParseCV(List<(string name, Vector3 ray)> inputs)
+    {
+        foreach(var p in inputs)
+        {
+            Vector3 relativeVector = transform.up * p.ray.y + -transform.right * p.ray.x + transform.forward * p.ray.z;
+            //if food, set food stuff
+            if (p.name == "cake" || p.name == "donut")
+            {
+                //set food vector to RELATIVE vector using up/left/right vectors
+                toFood = relativeVector;
+            }
+            else
+            {
+                //if it's not food, avoid the obstacle!
+                Vector3 avoid = velocity - relativeVector;
+                avoidObstacle = avoid;
+            }
+        }
+    }
     void CalculateVelocity()
     {
         toFood = findFood();
         velocity = Vector3.zero;
         cohesion = Vector3.zero;
         separation = Vector3.zero;
-        separationCount = 0;
         alignment = Vector3.zero;
         boids = Physics.OverlapSphere(transform.position, cohesionRadius);
         List<Collider> myBoids = new List<Collider>();
@@ -132,22 +169,79 @@ public class Boid : MonoBehaviour
         // Debug.Log(cohesion);
         separation = CalculateSeparation(myBoids);
         alignment = CalculateAlignment(myBoids);
-        velocity = cohesion * .2f + separation + alignment * 1.2f + toFood * .3f;
-    }
+        Vector3 upVec = new Vector3(0,1,0);
+        velocity = cohesion * .2f + separation + alignment * 1.7f+ toFood * .3f + upVec;
+        Vector3 land = LandingVec();
+        if (landing)
+        {
+            velocity = land;
+            //Debug.Log(land);
+        }
+        timeSincePerch += .1f;
 
+        //if perching, keep still
+        if (perching)
+        {
+            // Debug.Log(gameObject.name + " I'm perching!" );
+            velocity = Vector3.zero;
+            if (timeSincePerch > 5f)
+            {
+                perching = false;
+            }
+        }
+        velocity = velocity * .8f;
+    }
+    //ensure that boids do not go through the ground
+    void checkGround()
+    {
+        if(transform.position.y < -24)
+        {
+            transform.position = new Vector3(transform.position.x, -24, transform.position.z);
+        }
+    }
+    Vector3 LandingVec()
+    {
+        //check if close to groundHeight and velocity pointing downwards
+        //also check that this boid hasn't been to the ground recently
+        //if so, contribute a down vector inversely proportional to the distance from the ground
+        float heightDiff = (transform.position.y - groundHeight);
+        //Debug.Log(heightDiff);
+        toLand = new Vector3(0, -heightDiff / 3 - 2f, 0);
+        if(heightDiff < 1.0f && timeSincePerch > 10f)
+        {
+            if(perching == false)
+            {
+                timeSincePerch = 0;
+                perching = true;
+                Debug.Log("perch!");
+            }
+            landing = false;
+        }
+        toLand.x = velocity.x / 2;
+        toLand.z = velocity.z / 2;
+        return toLand;
+
+    }
     void Update()
     {
         if ((transform.position - origin).magnitude > maxDistance)
         {
-            velocity += -transform.position.normalized * 5;
+            velocity += -transform.position.normalized * 30;
         }
         transform.position += velocity * Time.deltaTime;
-        //transform.rotation = Quaternion.LookRotation(velocity);
+        checkGround();
+        timeUntilTired -= Time.deltaTime;
+        if(timeUntilTired <= 0f)
+        {
+            timeUntilTired = UnityEngine.Random.Range(25f, 100f);
+            landing = true;
+        }
+        transform.rotation = Quaternion.LookRotation(velocity);
 
-        Debug.DrawRay(transform.position, alignment, Color.blue);
-        Debug.DrawRay(transform.position, separation, Color.green);
-        Debug.DrawRay(transform.position, cohesion, Color.magenta);
-        Debug.DrawRay(transform.position, velocity, Color.yellow);
-        Debug.DrawRay(transform.position, toFood, Color.cyan);
+        //Debug.DrawRay(transform.position, alignment, Color.blue);
+        //Debug.DrawRay(transform.position, separation, Color.green);
+        //Debug.DrawRay(transform.position, cohesion, Color.magenta);
+        //Debug.DrawRay(transform.position, velocity, Color.yellow);
+        //Debug.DrawRay(transform.position, toFood, Color.cyan);
     }
 }
---
+++ Boids/Assets/Scripts/FoodScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //number of "pecks" it can take from boids
+    public int sizeOfFood = 20;
+    public int currentSize;
+    void Start()
+    {
+        currentSize = sizeOfFood;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentSize < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void getPecked()
+    {
+        currentSize--;
+    }
+}
